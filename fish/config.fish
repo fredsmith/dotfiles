5c8c5e8d909537c8cb74db07e38bd6f2fa50a36f
Fred Smith <fsmith@wanderu.com>
2025-02-18 20:22:27 -0500

    add environment config

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,8 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    # environment
+    set TZ US/Eastern
+    set FULLNAME "Fred Smith"
+    set EMAIL "fred@smith.bz"
+    set CONFIGDIR "$HOME/src/github.com/fredsmith/dotfiles";
+    set XDG_CONFIG_HOME $CONFIGDIR;
 end
