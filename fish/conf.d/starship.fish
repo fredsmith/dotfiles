7be1fdd7529fae4094392b8e29b22887ee9e8c5f
Fred Smith <fsmith@wanderu.com>
2025-02-22 10:23:13 -0500

    use starship config from dotfiles

--- fish/conf.d/starship.fish
+++ fish/conf.d/starship.fish
@@ -1,4 +1,5 @@
 if status is-interactive
+    set -x STARSHIP_CONFIG ~/src/github.com/fredsmith/dotfiles/starship.toml
     starship init fish | source
     # Commands to run in interactive sessions can go here
 end
