c5be903c75fcffeb660409ebfa3cbc07f0752a15
Fred Smith <fsmith@wanderu.com>
2026-05-27 13:08:44 -0400

    add git worktree functionality

--- fish/conf.d/git.fish
+++ fish/conf.d/git.fish
@@ -22,3 +22,30 @@ function gco
   end
   cd "$REPONAME"
 end
+
+
+function gwt
+  if test (count $argv) -eq 0
+    git worktree list
+    return
+  end
+  set BRANCHNAME $argv[1]
+  set REPOROOT (git rev-parse --show-toplevel 2>/dev/null)
+  if test -z "$REPOROOT"
+    echo "Not in a git repo"
+    return 1
+  end
+  # branch names can contain /, which we don't want as nested dirs
+  set SAFENAME (string replace -a '/' '-' $BRANCHNAME)
+  set WTPATH "$REPOROOT/.worktrees/$SAFENAME"
+
+  git fetch --all
+  if git show-ref --verify --quiet "refs/heads/$BRANCHNAME"
+    git worktree add "$WTPATH" "$BRANCHNAME"
+  else if git show-ref --verify --quiet "refs/remotes/origin/$BRANCHNAME"
+    git worktree add "$WTPATH" "$BRANCHNAME"
+  else
+    git worktree add "$WTPATH" -b "$BRANCHNAME"
+  end
+  cd "$WTPATH"
+end
